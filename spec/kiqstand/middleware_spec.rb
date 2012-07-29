154e02ba8fe838f384a54d0ff59aa3133fe6ca72
Durran Jordan <durran@gmail.com>
2012-07-29 22:02:47 +0200

    Ensure sessions are disconnected

--- spec/kiqstand/middleware_spec.rb
+++ spec/kiqstand/middleware_spec.rb
@@ -2,7 +2,7 @@ require "spec_helper"
 
 describe Kiqstand::Middleware do
 
-  class Band
+  class Model
     include Mongoid::Document
   end
 
@@ -15,12 +15,32 @@ describe Kiqstand::Middleware do
     context "when the identity map has items" do
 
       before do
-        Mongoid::IdentityMap.set(Band.new)
+        Mongoid::IdentityMap.set(Model.new)
         middleware.call {}
       end
 
       it "clears the identity map" do
         Mongoid::IdentityMap.should be_empty
+      end
+    end
+
+    context "when a session is alive" do
+
+      before do
+        Model.create
+        middleware.call {}
+      end
+
+      let(:sessions) do
+        Mongoid::Threaded.sessions.values
+      end
+
+      it "disconnects the session" do
+        sessions.each do |session|
+          session.cluster.nodes.each do |node|
+            node.send(:connected?).should be_false
+          end
+        end
       end
     end
   end
