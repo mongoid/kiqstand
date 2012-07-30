7542d94470983b56fd58a658bf41587b01a46b93
Durran Jordan <durran@gmail.com>
2012-07-30 23:48:48 +0200

    Master, 1.1.0, will use Mongoid 1.1.0 and better syntax

--- lib/kiqstand/middleware.rb
+++ lib/kiqstand/middleware.rb
@@ -2,26 +2,25 @@
 module Kiqstand
 
   # This is the middleware for ensuring Moped sessions are diconnected after
-  # a worker runs.
+  # a worker runs, and that the identity map is cleared.
   class Middleware
 
     # Ensures that after each worker runs, the identity map is cleared in case
     # it was accidentally enabled in this environment, and each session
     # disconnects it's nodes.
+    #
+    # @example Execute the worker.
+    #   worker.call
+    #
+    # @param [ Array ] args The arguments for the worker.
+    #
+    # @since 1.0.0
     def call(*args)
       yield
     ensure
       if defined?(::Mongoid)
         ::Mongoid::IdentityMap.clear
-        disconnect_sessions
-      end
-    end
-
-    private
-
-    def disconnect_sessions
-      ::Mongoid::Threaded.sessions.each_pair do |_, session|
-        session.disconnect
+        ::Mongoid.disconnect_sessions
       end
     end
   end
--- lib/kiqstand/version.rb
+++ lib/kiqstand/version.rb
@@ -1,4 +1,4 @@
 # encoding: utf-8
 module Kiqstand
-  VERSION = "1.0.0"
+  VERSION = "1.1.0"
 end
